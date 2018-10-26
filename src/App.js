488f296ad8bf83a70c872c93b0a105b59a3c1280
김영도 <younkng@gim-yeongdos-MacBook-Pro.local>
2018-10-26 18:48:52 -0400

    fixing App.js to make login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      email:'',
+      password:''
+    }
+  }
+
+  handleEmail(text)
+  {
+    this.setState({email:text.target.value})
+  }
+
+  handlePassword(text) {
+    this.setState({password:text.target.value})
+  }
+
+  login() {
+    console.warn("all state")
+    let obj = {}
+    obj.email = this.state.email
+    obj.password = this.state.password
+
+    fetch('http://localhost',
+      {
+      header: {
+        "Content-Type": "application/json"
+      },
+      method: 'POST',
+      body: JSON.stringify({obj})
+      }
+    )
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Login with React</h1>
         </header>
+
+        <input type="text" placeholder="Enter Email" onChange={(text) => { this.handleEmail(text) }} />
+        <br/>
+        <input type="password" placeholder="Enter Password" onChange={(text) => { this.handlePassword(text) }} />
+        <br/>
+        <button onClick={()=>{this.login()}}>Login</button> 
       </div>
     );
   }
